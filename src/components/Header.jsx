80c5f77260e6f86f0321a262cc5f36bef6cd0d55
MichaelVenturi <michaelventuri58@aol.com>
2023-03-27 16:24:09 -0400

    making a navbar with SASS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+import "./header.scss";
 
 const Header = () => {
-  return (
-    // instead of process.env like in CRA, use import.meta.env
-    <div>{import.meta.env.VITE_API_URL}</div>
-  )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default Header
+  const handleClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className="header">
+      <div className="header-content">
+        {/* instead of process.env like in CRA, use import.meta.env */}
+        <div className="header-content-title">
+          <h2>{import.meta.env.VITE_FAKE_VAR}</h2>
+        </div>
+        <nav className={`header-content-nav ${menuOpen ? "isMenu" : ""}`}>
+          <ul>
+            <li>
+              <span className="link">page 1</span>
+            </li>
+            <li>
+              <span className="link">page 2</span>
+            </li>
+            <li>
+              <span className="link">page 3</span>
+            </li>
+          </ul>
+          <button>CTA Page</button>
+        </nav>
+        <div className="header-content-toggle">
+          {!menuOpen ? (
+            <BiMenuAltRight onClick={handleClick} />
+          ) : (
+            <AiOutlineClose onClick={handleClick} />
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
