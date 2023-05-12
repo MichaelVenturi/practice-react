09b48595b32c9b3e3ddb526f16ce424f713de510
Michael <michaelventuri58@aol.com>
2023-05-12 19:19:02 -0400

    mongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
+import connectDB from "./db.js";
 
 config();
 
@@ -9,6 +10,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.status(200).json({ message: "ayo" });
 });
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
